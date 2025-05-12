8c674dbabc12b148b20bcde84b7ed892b1a19f77
ali1mughal <dgxali415@gmail.com>
2025-05-12 08:27:53 +0500

    index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const WebSocket = require('ws');
 const { Client, GatewayIntentBits } = require('discord.js');
-const fetch = require('node-fetch');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 require('dotenv').config();
 
 const app = express();
