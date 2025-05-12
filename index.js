92b4beb53f66ff5513335bbfd21b053b55a8d4c9
ali1mughal <dgxali415@gmail.com>
2025-05-12 12:37:24 +0500

    index.js

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const cors = require('cors');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const { createHash } = require('crypto');
-const PresenceManager = require('./PresenceManager.js');
-
+const PresenceManager = require('./PresenceManager'); // correct path
+const presenceManager = new PresenceManager(dataStores, config);
 
 // Configuration
 const config = {
